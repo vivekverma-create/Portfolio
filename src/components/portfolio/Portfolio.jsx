d06e29a545a4da5bf20a293211296c7527650d46
vivek <vivek@anu.edu.au>
2021-08-09 13:07:43 +1000

    links added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -80,13 +80,15 @@ export default function Portfolio() {
       </ul>
       <div className="container">
         {data.map((d) => (
-          <div className="item">
-            <img
-              src={d.img}
-              alt=""
-            />
-            <h3>{d.title}</h3>
-          </div>
+          <a href={d.link} target="_blank">
+            <div className="item">
+
+              <img
+                src={d.img}
+                alt=""
+              />
+              <h3>{d.title}</h3>
+            </div></a>
         ))}
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,104 +1,115 @@
 export const programmingPortfolio = [
-    {
-      id: 1,
-      title: "RailRoad Ink",
-      img:
-        "assets/railroad-ink.jpg",
-    },
-    {
-      id: 2,
-      title: "Car Classification",
-      img:
-        "assets/car-classification.png",
-    },
+  {
+    id: 1,
+    title: "RailRoad Ink",
+    img:
+      "assets/railroad-ink.jpg",
+    link: "https://drive.google.com/file/d/1LhyKG5dDLSPy-_b4rLi6Tf5mkKTH3Iw3/view?usp=sharing"
+  },
+  {
+    id: 2,
+    title: "Car Classification",
+    img:
+      "assets/car-classification.png",
+    link: "http://ai.stanford.edu/~jkrause/cars/car_dataset.html"
+  },
 
-  ];
-  
-  export const webPortfolio = [
-    {
-      id: 1,
-      title: "NBA Stats Explorer",
-      img: "assets/NBA.jpg",
-    },
-    {
-      id: 2,
-      title: "SP500 Stocks",
-      img: "assets/stocks.jpg",
-    },
-    {
-      id: 3,
-      title: "Covid-19",
-      img: "assets/covid.jpg",
-    },
-    {
-      id: 4,
-      title: "Data Checker",
-      img: "assets/data-checker.jpg",
-    },
-  ];
-  
-  export const aeromodellingPortfolio = [
-    {
-      id: 1,
-      title: "Quadcopter",
-      img:
-        "assets/quad.jpg",
-    },
-    {
-      id: 2,
-      title: "Electric Plane",
-      img:
-        "assets/toyPlane.jpg",
-    },
-    {
-      id: 3,
-      title: "Engine Plane",
-      img:
-        "assets/aero-modelling.jpg",
-    }
-  ];
-  
-  export const electronicsPortfolio = [
-    {
-      id: 1,
-      title: "Smart Switch",
-      img:
-        "assets/smart-switch.jpg",
-    },
-    {
-      id: 2,
-      title: "ThermoElectric Generator",
-      img:
-        "assets/electronics.jpg",
-    },
+];
 
-  ];
-  
-  export const roboticsPortfolio = [
-    {
-      id: 1,
-      title: "Spider",
-      img:
-        "assets/spider.jpg",
-    },
-    {
-      id: 2,
-      title: "Navigator",
-      img:
-        "assets/robotics.jpg",
-    }
-  ];
-  export const researchPortfolio = [
-    {
-      id: 1,
-      title: "Magnessium Ferrite",
-      img:
-        "assets/paper.jpg",
-    },
-    {
-      id: 2,
-      title: "Multi-Ferroics",
-      img:
-        "assets/research.jpg",
-    }
-  ];
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "NBA Stats Explorer",
+    img: "assets/NBA.jpg",
+    link: "http://visualise-data.herokuapp.com/",
+  },
+  {
+    id: 2,
+    title: "SP500 Stocks",
+    img: "assets/stocks.jpg",
+    link: "http://visualise-data.herokuapp.com/",
+  },
+  {
+    id: 3,
+    title: "Covid-19",
+    img: "assets/covid.jpg",
+    link: "http://visualise-data.herokuapp.com/",
+  },
+  {
+    id: 4,
+    title: "Data Checker",
+    img: "assets/data-checker.jpg",
+    link: "http://visualise-data.herokuapp.com/",
+  },
+];
+
+export const aeromodellingPortfolio = [
+  {
+    id: 1,
+    title: "Quadcopter",
+    img:
+      "assets/quad.jpg",
+  },
+  {
+    id: 2,
+    title: "Electric Plane",
+    img:
+      "assets/toyPlane.jpg",
+  },
+  {
+    id: 3,
+    title: "Engine Plane",
+    img:
+      "assets/aero-modelling.jpg",
+  }
+];
+
+export const electronicsPortfolio = [
+  {
+    id: 1,
+    title: "Smart Switch",
+    img:
+      "assets/smart-switch.jpg",
+      link:"https://drive.google.com/file/d/1qlJgkzuUviXgQJih1Yrd_tQJAyRDvjFI/view?usp=sharing"
+  },
+  {
+    id: 2,
+    title: "ThermoElectric Generator",
+    img:
+      "assets/electronics.jpg",
+      link:"https://drive.google.com/drive/folders/1Kp46KhNzk5uCoK2JoHyo4EcnQH3qpQiU?usp=sharing"
+  },
+
+];
+
+export const roboticsPortfolio = [
+  {
+    id: 1,
+    title: "Spider",
+    img:
+      "assets/spider.jpg",
+      link:"https://drive.google.com/drive/folders/1ix6A_YXMDQ6nEaSUkTy0yWG4GQVxCIMb?usp=sharing"
+  },
+  {
+    id: 2,
+    title: "Navigator",
+    img:
+      "assets/robotics.jpg",
+  }
+];
+export const researchPortfolio = [
+  {
+    id: 1,
+    title: "Magnesium Ferrite",
+    img:
+      "assets/paper.jpg",
+      link:"https://drive.google.com/file/d/1IE4PM76iqLMkhn1zOetgLZpy1DxQuQ7i/view?usp=sharing"
+  },
+  {
+    id: 2,
+    title: "Multi-Ferroics",
+    img:
+      "assets/research.jpg",
+      link:"https://drive.google.com/file/d/1AmlO6vKtbiUg35LK2CfYLYJ9zEKDljbc/view?usp=sharing"
+  }
+];
