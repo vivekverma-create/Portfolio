247bd247b423013bf225f2db8ed9f262f4146996
vivek <vivek@anu.edu.au>
2022-10-15 21:10:54 +1100

    changes to metadata

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="/Portfolio" className="logo">
+          <a href="/" className="logo">
             Vivek.
           </a>
           <div className="itemContainer">
