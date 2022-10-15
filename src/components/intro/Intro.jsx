8a106601b1c408d36a4b31fec93b3fd195ff4562
vivek <vivek@anu.edu.au>
2022-10-15 21:28:07 +1100

    changes to metadata

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -10,7 +10,7 @@ export default function Intro() {
       showCursor: true,
       backDelay: 1500,
       backSpeed:60,
-      strings: ["Developer","Engineer","Tech Enthusiast"],
+      strings: ["(Developer)","(Engineer)","(Tech Enthusiast)"],
     });
   }, []);
 
